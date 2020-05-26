074661b46080e763f525e60761f92986b8aa48b2
ludeeus <joasoe@gmail.com>
2020-05-26 17:06:07 +0000

    Add checks for fork and owner

---
+++ scripts/check/owner.py
@@ -0,0 +1,27 @@
+import asyncio
+import json
+import os
+from glob import glob
+
+from aiogithubapi import GitHub
+from scripts.changed.repo import get_repo
+
+TOKEN = os.getenv("GITHUB_TOKEN")
+ACTOR = os.getenv("GITHUB_ACTOR", "ludeeus")
+
+
+async def check():
+    print("Information: https://hacs.xyz/docs/publish/include#check-owner")
+    repo = get_repo()
+    async with GitHub(TOKEN) as github:
+        repository = await github.get_repo(repo)
+        repo = repository.attributes
+
+    if ACTOR == repo["full_name"].split("/")[0]:
+        print(f"{ACTOR} is the owner of the repository")
+        return
+
+    exit(1)
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(check())
