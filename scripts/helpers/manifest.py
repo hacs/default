dd41ef803bfe613256a0b55a87db584ee4788784
ludeeus <joasoe@gmail.com>
2020-05-26 15:19:44 +0000

    Fix domain

--- scripts/helpers/manifest.py
+++ scripts/helpers/manifest.py
@@ -5,8 +5,6 @@ from scripts.helpers.integration_path import get_integration_path
 
 def get_manifest():
     manifest = f"{get_integration_path()}/manifest.json"
-    print(f"Found manifest at {manifest.replace('/tmp/addition','')}")
-
     with open(manifest, "r") as mf:
         manifest = json.loads(mf.read())
 
