b16345f52594dee48643f3cb8f1b9c3a68a417d2
ludeeus <joasoe@gmail.com>
2020-05-26 06:23:27 +0000

    Test new check

---
+++ scripts/changed/repo.py
@@ -0,0 +1,26 @@
+import json
+from .category import get_category
+
+DEFAULT = "/tmp/default"
+
+
+def get_repo():
+    category = get_category()
+    with open(f"{DEFAULT}/{category}", "r") as default:
+        current = json.loads(default.read())
+
+    with open(category, "r") as default:
+        new = json.loads(default.read())
+
+    for repo in current:
+        if repo in new:
+            new.remove(repo)
+
+    if len(new) != 1:
+        print(f"Bad data {new}")
+        exit(1)
+
+    return new.pop()
+
+if __name__ == "__main__":
+    print(get_repo())
