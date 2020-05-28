bc0702be78791c22a5ca7acf2ef961d92800434b
ludeeus <joasoe@gmail.com>
2020-05-28 07:17:57 +0000

    get event

---
+++ scripts/check/should_run.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+def check():
+    with open(os.getenv("GITHUB_EVENT_PATH"), "r") as event_data:
+        event = json.loads(event_data.read())
+
+    print(event)
+    return True
+
+
+if __name__ == "__main__":
+    print(check())
