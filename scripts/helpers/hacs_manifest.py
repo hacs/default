42f95517aa4d8f16bdf159b633f3114551a9b826
ludeeus <joasoe@gmail.com>
2020-05-28 06:13:19 +0000

    Add preflight

--- scripts/helpers/hacs_manifest.py
+++ scripts/helpers/hacs_manifest.py
@@ -5,7 +5,7 @@ from glob import glob
 
 def get_hacs_manifest():
     files = []
-    for dir, _, _ in os.walk("/tmp/addition"):
+    for dir, _, _ in os.walk("/tmp/repositories/addition"):
         files.extend(glob(os.path.join(dir, "*hacs.json")))
 
     if len(files) != 1:
@@ -13,7 +13,7 @@ def get_hacs_manifest():
         exit(1)
 
     hacs_manifest = files.pop()
-    print(f"Found HACS manifest at {hacs_manifest.replace('/tmp/addition','')}")
+    print(f"Found HACS manifest at {hacs_manifest.replace('/tmp/repositories/addition','')}")
 
     with open(hacs_manifest, "r") as mf:
         hacs_manifest = json.loads(mf.read())
