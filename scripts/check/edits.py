d960254ef12340c9c01e91fc91738d3f343e483b
Ludeeus <ludeeus@ludeeus.dev>
2021-03-13 13:58:10 +0000

    f

--- scripts/check/edits.py
+++ scripts/check/edits.py
@@ -4,11 +4,10 @@ from scripts.helpers.event import get_event
 
 
 async def check():
-
     event = get_event()
     pull_request = event["pull_request"]
     if not pull_request["maintainer_can_modify"]:
-        if not pull_request["head"]["full_name"] != "hacs/default":
+        if not pull_request["head"]["repo"]["full_name"] != "hacs/default":
             exit("::error::The PR is not editable by HACS maintainers")
 
 
